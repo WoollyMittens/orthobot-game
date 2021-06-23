3c9004e804d5966e84ad2f09f6d714d0f1b31f45
Maurice van Creij <maurice@woollymittens.nl>
2021-06-23 22:11:41 +1000

    Optimise unrender logic

--- src/map/tile.js
+++ src/map/tile.js
@@ -15,14 +15,17 @@ export class Tile {
         var tile = document.createElement("div");
         var col = index % this.model.colcount;
         var row = Math.floor(index / this.model.colcount);
+        this.x = col * this.model.gridsize;
+        this.y =  row * this.model.gridsize * this.model.foreshorten;
+        this.z = this.y + 1;
         // common properties
         tile.setAttribute("class", "ob-tile");
         tile.setAttribute("data-variant", char);
         tile.setAttribute("data-row", row);
         tile.setAttribute("data-col", col);
-        tile.setAttribute("data-x",  col * this.model.gridsize);
-        tile.setAttribute("data-y", row * this.model.gridsize * this.model.foreshorten);
-        tile.setAttribute("data-z", row * this.model.gridsize * this.model.foreshorten + 1);
+        tile.setAttribute("data-x",  this.x);
+        tile.setAttribute("data-y", this.y);
+        tile.setAttribute("data-z", this.z);
         for (var key in attributes["common"]) {
             tile.setAttribute("data-" + key, attributes["common"][key]);
         }
@@ -30,6 +33,10 @@ export class Tile {
         for (var key in attributes[char]) {
             tile.setAttribute("data-" + key, attributes[char][key]);
         }
+        // constant styles
+        tile.style.transform = `translate3d(${this.x}px, ${this.y}px, ${this.z}px)`;
+        tile.style.width = this.model.gridsize + "px";
+        tile.style.height = this.model.gridsize + "px";
         // add the tile to the background layer
         this.model.background.appendChild(tile);
         // add the tile to the model
@@ -43,23 +50,18 @@ export class Tile {
         // check if any conditions have been met
     }
 
-    //  TODO: do this with get and set
-    attr = function (element, attribute) {
-        return parseFloat(element.getAttribute("data-" + attribute));
-    }
-
-    outofbounds = function (tx, ty) {
+    render = function () {
         // check visibility
         var bg = this.model.background;
         var vp = this.model.viewport;
         var grid = this.model.gridsize;
-        var bgx = this.attr(bg, "x");
-        var bgy = this.attr(bg, "y");
+        var bgx = parseFloat(bg.getAttribute("data-x"));
+        var bgy = parseFloat(bg.getAttribute("data-y"));
         var tr = {
-            "top": ty + bgy,
-            "right": tx + grid + bgx,
-            "bottom": ty + grid + bgy,
-            "left": tx + bgx,
+            "top": this.y + bgy,
+            "right": this.x + grid + bgx,
+            "bottom": this.y + grid + bgy,
+            "left": this.x + bgx,
         };
         var vr = {
             "top": 0,
@@ -67,19 +69,9 @@ export class Tile {
             "bottom": vp.offsetHeight,
             "left": 0
         };
-        return (tr.bottom < vr.top || tr.left > vr.right || tr.top > vr.bottom || tr.right < vr.left);
-    }
-
-    render = function () {
-        var tx = this.attr(this.element, "x");
-        var ty = this.attr(this.element, "y");
-        var tz = this.attr(this.element, "z");
-        // position the tile
+        // (un)render the tile
         Object.assign(this.element.style, {
-            width: this.model.gridsize + "px",
-            height: this.model.gridsize + "px",
-            display: (this.outofbounds(tx, ty)) ? "none" : "block",
-            transform: `translate3d(${tx}px, ${ty}px, ${tz}px)`
+            display: (tr.bottom < vr.top || tr.left > vr.right || tr.top > vr.bottom || tr.right < vr.left) ? "none" : "block"
         });
     }
 
